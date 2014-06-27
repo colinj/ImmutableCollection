8941ffab5121a2e525886cddbb833e5f86a5082a
Colin Johnsun <colin.johnsun@gmail.com>
2014-06-27 21:16:42 +1000

    Add license details

--- src/ImmutableCollections.pas
+++ src/ImmutableCollections.pas
@@ -1,3 +1,22 @@
+{****************************************************}
+{                                                    }
+{  ImmutableCollections                              }
+{                                                    }
+{  Copyright (C) 2014 Colin Johnsun                  }
+{                                                    }
+{  https://github.com/colinj/ImmutableCollection     }
+{                                                    }
+{****************************************************}
+{                                                    }
+{  This Source Code Form is subject to the terms of  }
+{  the Mozilla Public License, v. 2.0. If a copy of  }
+{  the MPL was not distributed with this file, You   }
+{  can obtain one at                                 }
+{                                                    }
+{  http://mozilla.org/MPL/2.0/                       }
+{                                                    }
+{****************************************************}
+
 unit ImmutableCollections;
 
 interface
